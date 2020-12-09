8f710ba30464a6720bb4090ae21a89bf602ee3a5
Stijn <stijn@ardusit.nl>
2020-12-10 00:47:36 +0100

    Commit #3
    Remade some of the alerts
    Added styling and background
    Added a button to start ordering
    Beautified the html and js

--- script.js
+++ script.js
@@ -2,70 +2,70 @@
 // Software Developer 2020
 
 var orders = {
-    soda : 0,
-    beer : 0,
-    wine : 0,
-    bowl8 : 0,
-    bowl16 : 0
+    soda: 0,
+    beer: 0,
+    wine: 0,
+    bowl8: 0,
+    bowl16: 0
 }
 
 var price = {
-    soda : 1.50,
-    beer : 2.00,
-    wine : 3.00,
-    bowl8 : 4.50,
-    bowl16 : 9.00
+    soda: 1.50,
+    beer: 2.00,
+    wine: 3.00,
+    bowl8: 4.50,
+    bowl16: 9.00
 }
 
 var total = {
-    soda : 0,
-    beer : 0,
-    wine : 0,
-    bowl8 : 0,
-    bowl16 : 0
+    soda: 0,
+    beer: 0,
+    wine: 0,
+    bowl8: 0,
+    bowl16: 0
 }
 
 var check = {
-    soda : false,
-    beer : false,
-    wine : false,
-    bowl8 : false,
-    bowl16 : false
+    soda: false,
+    beer: false,
+    wine: false,
+    bowl8: false,
+    bowl16: false
 }
 
-orderStatus = true ;
+orderStatus = true;
 
-order();
-
-function order(){
-    orderStatus = true ;
-    while(orderStatus === true){
-        var quest = prompt('Menu\n \n Soda\n Beer\n Wine\n Snacks \n \n To get the bill type \n Stop').toLowerCase();
-        switch(quest){
-            case 'stop' :
+function order() {
+    document.getElementById("button").disabled = true;
+    alert('Welcome to The Unnamed pub\n\nDue to the ongoing Covid-19 pandemic you can now temporarily order online!\n\nClick OK to see our menu')
+    orderStatus = true;
+    while (orderStatus === true) {
+        var quest = prompt('Menu\n \n Soda €1,50\n Beer €2,00\n Wine €3,00\n Snacks \n \n To get the bill type \n Stop').toLowerCase();
+        switch (quest) {
+            case 'stop':
                 orderStatus = false;
                 let totalAmount = total.soda + total.beer + total.wine + total.bowl8 + total.bowl16;
                 document.getElementById('total').innerHTML = "You're total is " + totalAmount;
-                if(check.soda === true){
-                    document.getElementById('output').innerHTML += orders.soda + ' x '  + price.soda + ' = ' + total.soda + '<br>';
+                if (check.soda === true) {
+                    document.getElementById('output').innerHTML += orders.soda + ' soda x ' + price.soda + ' = ' + total.soda + '<br>';
                 }
-                if(check.beer === true){
-                    document.getElementById('output').innerHTML += orders.beer + ' x '  + price.beer + ' = ' + total.beer + '<br>';
+                if (check.beer === true) {
+                    document.getElementById('output').innerHTML += orders.beer + ' beer x ' + price.beer + ' = ' + total.beer + '<br>';
                 }
-                if(check.wine === true){
-                    document.getElementById('output').innerHTML += orders.wine + ' x '  + price.wine + ' = ' + total.wine + '<br>';
+                if (check.wine === true) {
+                    document.getElementById('output').innerHTML += orders.wine + ' wine x ' + price.wine + ' = ' + total.wine + '<br>';
                 }
-                if(check.bowl8 === true){
-                    document.getElementById('output').innerHTML += orders.bowl8 + ' x '  + price.bowl8 + ' = ' + total.bowl8 + '<br>';
+                if (check.bowl8 === true) {
+                    document.getElementById('output').innerHTML += orders.bowl8 + ' bowls of 8 x ' + price.bowl8 + ' = ' + total.bowl8 + '<br>';
                 }
-                if(check.bowl16 === true){
-                    document.getElementById('output').innerHTML += orders.bowl16 + ' x '  + price.bowl16 + ' = ' + total.bowl16 + '<br>';
+                if (check.bowl16 === true) {
+                    document.getElementById('output').innerHTML += orders.bowl16 + ' bowls of 16 x ' + price.bowl16 + ' = ' + total.bowl16 + '<br>';
                 }
                 break;
-            case 'soda' :
+            case 'soda':
                 let sodaQuest = prompt('How many sodas do you want');
                 parseInt(sodaQuest);
-                if(isNaN(sodaQuest) || sodaQuest == 0){
+                if (isNaN(sodaQuest) || sodaQuest == 0) {
                     alert('You didnt order any sodas')
                 } else {
                     check.soda = true;
@@ -74,10 +74,10 @@ function order(){
                     alert('You ordered ' + sodaQuest + " soda's")
                 }
                 break;
-            case 'beer' :
+            case 'beer':
                 let beerQuest = prompt('How many beers do you want?');
                 parseInt(beerQuest);
-                if(isNaN(beerQuest) || beerQuest == 0){
+                if (isNaN(beerQuest) || beerQuest == 0) {
                     alert('You didnt order any beer')
                 } else {
                     check.beer = true;
@@ -86,10 +86,10 @@ function order(){
                     alert('You ordered ' + beerQuest + ' beers')
                 }
                 break;
-            case 'wine' :
+            case 'wine':
                 let wineQuest = prompt('How many wines do you want?');
                 parseInt(wineQuest);
-                if(isNaN(wineQuest) || wineQuest == 0){
+                if (isNaN(wineQuest) || wineQuest == 0) {
                     alert('You didnt order any wines')
                 } else {
                     check.wine = true;
@@ -97,22 +97,21 @@ function order(){
                     total.wine = orders.wine * price.wine;
                     alert('You ordered ' + wineQuest + ' wine');
                 }
-                break;   
-            case 'snack':
-                let snackQuest = prompt('Do you want a bowl of 8 or 16 snacks?');
+                break;
+            case 'snacks':
+                let snackQuest = prompt('Do you want a bowl of 8 or 16 snacks?\n\nBowl of 8 €4,50\nBowl of 16 €9,00');
                 parseInt(snackQuest);
-                if(snackQuest == 8 || snackQuest == 16){
-                    alert('uwu');
-                    if(snackQuest == 8){
+                if (snackQuest == 8 || snackQuest == 16) {
+                    if (snackQuest == 8) {
                         let amount8 = prompt('How many bowls of 8 snacks do you want?');
                         check.bowl8 = true;
                         orders.bowl8 = amount8;
                         total.bowl8 = orders.bowl8 * price.bowl8;
                         alert('You ordered ' + amount8 + ' bowls of 8 snacks');
                     }
-                    if(snackQuest == 16){
+                    if (snackQuest == 16) {
                         check.bowl16 = true;
-                        let amount16 = prompt('How many bowls of 16 snacks do you want') ;
+                        let amount16 = prompt('How many bowls of 16 snacks do you want?');
                         orders.bowl16 = amount16;
                         total.bowl16 = orders.bowl16 * price.bowl16;
                         alert('You ordered ' + amount16 + ' bowls of 16 snacks');
